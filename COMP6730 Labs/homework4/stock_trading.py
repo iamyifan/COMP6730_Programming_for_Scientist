f924810a98ebf7426ddc014903d0b99b2465b13a
Yifan Luo <yifan-luo@qq.com>
2022-09-11 19:18:01 +1000

    update homework4

--- COMP6730 Labs/homework4/stock_trading.py
+++ COMP6730 Labs/homework4/stock_trading.py
@@ -12,67 +12,58 @@
 import math
 
 def stock_trade(stock_price, capital, p):
-    '''
-    A simple trading strategy for buying and selling shares.
-    Given the starting cash and the prudence coefficient, the stock trader program will
-    use a fixed fraction (1-p) of available capital to buy stocks if the price increases.
-    Otherwise, sell a fraction (1-p) of shares owned. The program will return the profit/loss
-    according to its available cash and shares at the final stage,
-    i.e. (final cash + final share price) - starting cash.
+    ''' A simple trading strategy for buying and selling shares.
+    Given the starting cash and the prudence coefficient, the program will use a fixed fraction (1-p) of
+    available capital to buy stocks if the price increases; Otherwise, the program will sell a fraction (1-p)
+    of shares owned. The profit will be returned according to its available cash and shares at the end,
+    i.e. profit = (available cash + number of shares * last stock price) - starting cash.
 
     Parameters:
-        stock_price (sequence of numbers): stock price at each stage, a sequence of positive numbers
-        capital (float): starting cash used to buy shares, a positive number
-        p (float): prudence coefficient, range [0, 1]
+        stock_price (list[int/float]/tuple(int/float)): stock price at each stage, a sequence of positive numbers
+        capital (int/float): starting cash used to buy shares, a positive number
+        p (int/float): prudence coefficient, range [0, 1]
 
     Returns:
         profit (float): gain or loss in the value of the entire assets
     '''
 
-    assert type(stock_price) in [list, tuple], "the stock price has to be a sequence of numbers"
+    assert type(stock_price) in [list, tuple], "stock price has to be iterable"
     for sp in stock_price:
-        if type(sp) not in [int, float]:
-            print("stock price {} has to be a number".format(sp))
-            raise TypeError
-        if sp <= 0:
-            print("stock price {} is not positive".format(sp))
-            raise ValueError
-    assert type(capital) == float or type(capital) == int, "the capital has to be a number"
-    assert capital > 0, "the capital has to be positive"
-    assert type(p) == float or type(p) == int, "the prudence coefficient has to be a number"
-    assert 0 <= p <= 1, "the prudence coefficient has range [0, 1]"
+        assert type(sp) in [int, float], "stock price {} has to be a number".format(sp)
+        assert sp > 0, "stock price {} has to be positive".format(sp)
+    assert type(capital) in [int, float], "capital has to be a number"
+    assert capital > 0, "capital has to be positive"
+    assert type(p) in [int, float], "prudence coefficient has to be a number"
+    assert 0 <= p <= 1, "prudence coefficient has range [0, 1]"
 
-    curr_capital = capital  # used to represent the current capital at each stage
-    last_price = math.inf   # used to represent the share price of last time
-    curr_shares = 0         # used to calculate the current owned shares at each stage
+    curr_capital = capital  # available capital at each stage, init with starting capital
+    last_price = math.inf   # share price of last time, init with infinity for first-buy scenarios
+    curr_shares = 0         # owned shares at each stage, init with no shares
+
     for i in range(len(stock_price)):
-        if stock_price[i] > last_price:    # if the price increases, sell stocks
-            # the maximum current owned shares can be sold
-            curr_shares_sell = (curr_shares * (1 - p)) // 1
-            # update the current capital after this selling
-            curr_capital += curr_shares_sell * stock_price[i]
-            # update the current total number of shares
-            curr_shares -= curr_shares_sell
-        elif stock_price[i] < last_price:  # if the price decreases, buy stocks
-            curr_capital_available = round(curr_capital * (1 - p))
-            # buy stocks if the current available capital is sufficient,
-            # otherwise, only record the current stock price as the last stock price
-            if curr_capital_available >= stock_price[i]:
-                # calculate the maximum number of shares with the total cost not exceeding that fraction
-                curr_shares_buy = curr_capital_available // stock_price[i]
-                # update the current capital after this buying
-                curr_capital -= curr_shares_buy * stock_price[i]
-                # update the current total number of shares
-                curr_shares += curr_shares_buy
-        else:  # if the price doesn't change, jump to the next stage
-            continue
-        last_price = stock_price[i]
+        if stock_price[i] > last_price:          # sell stocks if price increases
+            n_shares_sell = (curr_shares * (1 - p)) // 1        # maximum shares can sell
+            curr_capital += n_shares_sell * stock_price[i]      # update available capital after selling
+            curr_shares -= n_shares_sell                        # update owned shares after selling
+        elif stock_price[i] < last_price:        # buy stocks if price decreases
+            n_capital_buy = round(curr_capital * (1 - p))       # maximum capital used to buy
+            if n_capital_buy >= stock_price[i]:  # buy stocks if available capital is enough
+                n_shares_buy = n_capital_buy // stock_price[i]  # maximum shares can buy
+                curr_capital -= n_shares_buy * stock_price[i]   # update available capital after buying
+                curr_shares += n_shares_buy                     # update owned shares after buying
+        # if owned shares == 0, no shares hold, need to buy shares before any selling
+        # (it happens in first-buy, or when some trades were done but no shares left)
+        # keep last price math.inf, wait until price drops low enough
+        # if owned shares > 0, trades happened, continually update last price
+        # (when price doesn't change, updating won't hurt, since price is same)
+        last_price = stock_price[i] if curr_shares > 0 else math.inf
 
-    # if the last price is a math.nan, it means the program never bought any stocks,
-    # and the profit will also be a math.nan. in that case, return a float 0.0
-    profit = (curr_capital + curr_shares * last_price) - capital
+    if curr_shares == 0:  # no shares hold, profit is capital's difference, whether trades happened or not
+        profit = float(curr_capital - capital)
+    else:                 # can't merge two equations, since 0 * math.inf == math.nan will have issues
+        profit = float((curr_capital + curr_shares * last_price) - capital)
 
-    return float(profit) if not math.isnan(profit) else float(0)
+    return profit
     
 
 def test_stock_trade():
