898d32079a53bc434cedbb966a6c409a82d2a400
Yifan Luo <yifan-luo@qq.com>
2022-10-08 17:46:38 +1100

    Update project.py

--- Group Project/project.py
+++ Group Project/project.py
@@ -4,22 +4,23 @@ The assignment specification is available on the course web
 site, at https://cs.anu.edu.au/courses/comp1730/assessment/project/
 
 Collaborators:
-u7015074, Tanya Barbbar
+u7015074, Tanya Babbar
 u7309356, Sam Eckton
 u7351505, Yifan Luo
 """
 
-import sys
-import isort
-import platform
 
 def task1():
+    import platform
+
     def get_stdlib_packages():
-        major, minor = sys.version_info.major, sys.version_info.minor  # get Python version majpr.minor
+        import sys, isort
+
+        major, minor = sys.version_info.major, sys.version_info.minor  # get Python version major.minor
         assert major == 3, "Python3 is used in this project."
 
         if 5 <= minor <= 9:
-            stdlibs = set(eval("isort.stdlibs.py39.stdlib"))
+            stdlibs = set(eval("isort.stdlibs.py3" + str(minor) + ".stdlib"))
         elif minor >= 10:
             stdlibs = set(sys.stdlib_module_names)
         else:
@@ -52,5 +53,5 @@ def analyse_stdlib():
 if __name__ == '__main__':
     NAME = 'Yifan Luo'
     ID   = 'u7351505'
-    print(f'My name is {NAME}, my id is {ID}, and these are my findings for Project COMP1730.2022.S2')
+    print(f'My name is {NAME}, my id is {ID}, and these are my findings for Project COMP6730.2022.S2')
     analyse_stdlib()
