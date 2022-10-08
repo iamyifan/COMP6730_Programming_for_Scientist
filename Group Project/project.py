cad02d742b6b872d88b8e22c653413af99bd2c59
Yifan Luo <yifan-luo@qq.com>
2022-10-09 00:13:31 +1100

    update task1 code

--- Group Project/project.py
+++ Group Project/project.py
@@ -11,39 +11,73 @@ u7351505, Yifan Luo
 
 
 def task1():
+    """
+    The function calls the function get_stdlib_packages and uses its
+    results (a set of StdLib package names) to print the Python version,
+    the OS name, and the first five and the last five StdLib package names.
+
+    Parameters:
+        No parameters.
+
+    Returns:
+        No returns.
+
+    Assumptions:
+        No assumptions.
+    """
     import platform
 
     def get_stdlib_packages():
-        import sys, isort
+        """
+        The function returns a set containing external StdLib package names
+        (without two packages "this" and "antigravity") according to different
+        Python versions.
 
-        major, minor = sys.version_info.major, sys.version_info.minor  # get Python version major.minor
+        Parameters:
+            No parameters.
+
+        Returns:
+            stdlibs (set): A set containing StdLib, without internal packages
+                and two external packages "this" and "antigravity".
+
+        Assumptions:
+            The function assume the program is working on Python3.5+.
+        """
+        import sys
+        import isort
+
+        # get the Python version major.minor, e.g. 3.8 (major = 3, minor = 8)
+        major, minor = sys.version_info.major, sys.version_info.minor
+
+        # check if the requirement of Python version is met
         assert major == 3, "Python3 is used in this project."
+        assert minor >= 5, "Python3.5+ is used in this project."
 
+        # get StdLib for each Python version
         if 5 <= minor <= 9:
             stdlibs = set(eval("isort.stdlibs.py3" + str(minor) + ".stdlib"))
         elif minor >= 10:
             stdlibs = set(sys.stdlib_module_names)
-        else:
-            raise Exception("Python3.5+ is used in this project.")
 
+        # remove internal packages, and substitute them with None
         stdlibs = set(map(lambda stdlib: None if stdlib[0] == '_' else stdlib, stdlibs))
-        stdlibs.discard(None)
-        stdlibs.discard("this")
-        stdlibs.discard("antigravity")
+        stdlibs.discard(None)            # remove None from the last step
+        stdlibs.discard("this")          # remove "this"
+        stdlibs.discard("antigravity")   # remove "antigravity"
 
         return stdlibs
 
-    stdlibs = sorted(list(get_stdlib_packages()))
-    os_name = platform.platform()
-    py_ver = platform.python_version()
+    stdlibs = sorted(list(get_stdlib_packages()))  # get sorted StdLib list
+    os_name = platform.platform()                  # get OS name
+    py_ver = platform.python_version()             # get Python version
+
     print("Python {py_ver} on {os_name}".format(py_ver=py_ver, os_name=os_name))
     print("StdLib contains {} external modules and packages:".format(len(stdlibs)))
-    print(stdlibs[:5] + stdlibs[-5:])
+    print(", ".join(stdlibs[:5]) + " ... " + ", ".join(stdlibs[-5:]))
 
 
 def analyse_stdlib():
     task1()
-    pass
 
 
 # The section below will be executed when you run this file.
@@ -52,6 +86,6 @@ def analyse_stdlib():
 
 if __name__ == '__main__':
     NAME = 'Yifan Luo'
-    ID   = 'u7351505'
+    ID = 'u7351505'
     print(f'My name is {NAME}, my id is {ID}, and these are my findings for Project COMP6730.2022.S2')
     analyse_stdlib()
